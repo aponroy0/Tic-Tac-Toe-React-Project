30e5b4ff60a410b2e89926a98d9b930c5926089d
Apon Roy <roy.apon.700@gmail.com>
2025-11-17 02:35:08 +0600

    Completed V-1.0

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board() {
+function Board({ squares, xIsNext, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -32,8 +32,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setxIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   return (
@@ -63,14 +62,44 @@ function Board() {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setxIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    setxIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setxIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move no :  ${move}`;
+    } else {
+      description = `Go to start the game. `;
+    }
+
+    return (
+      <li key={move} className="bg-gray-700 text-white mb-1 rounded-sm p-2">
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
-    <div>
-      <div>
-        <Game />
+    <div className="flex justify-center p-4">
+      <div className="mr-16">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div>
-        <ol></ol>
+        <ol className="border border-gray-400 p-1 text-lg">{moves}</ol>
       </div>
     </div>
   );
